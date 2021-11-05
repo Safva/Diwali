31720c3b501aa3450259edf3d2e67ae1b503a2e4
Safva <safva2000@gmail.com>
2021-11-05 20:03:03 +0530

    second commit

--- resources/views/diwalihome.blade.php
+++ resources/views/diwalihome.blade.php
@@ -6,7 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Happy Diwali</title>
-    <link rel="stylesheet" href="{{ asset('css/diwali_style.css')}}">
+    <!-- <link rel="stylesheet" href="{{ asset('css/diwali_style.css')}}"> -->
+    <link rel="stylesheet" type="text/css" href="{{asset('css/diwali_style.css')}}" />
 </head>
 
 <body>
